dac356f9c8e1a3823fd95d9782799f636a347cb3
whr1998 <2973347293@qq.com>
2023-02-01 16:41:31 +0800

    2023/2/1

--- src/layout/container/Container.vue
+++ src/layout/container/Container.vue
@@ -1,18 +1,69 @@
 <script setup lang="ts">
 import { ref, reactive } from 'vue';
+const info = reactive([
+  {
+    name: 'ESTELLE',
+    img: '../../../public/introduce/1.jpg',
+    baike: '',
+  },
+  {
+    name: 'ISHIHAR SATOMI',
+    img: '../../../public/introduce/1.jpg',
+    baike: '',
+  },
+  {
+    name: 'SONGYI',
+    img: '../../../public/introduce/1.jpg',
+    baike: '',
+  },
+  {
+    name: 'KIMCHAEHYUN',
+    img: '../../../public/introduce/1.jpg',
+    baike: '',
+  },
+]);
 </script>
 
 <template>
-  <div class="container">
+  <div class="container" v-for="item in info">
+    <div class="inner">
+      <div class="header">
+        {{ item.name }}
+      </div>
+      <div class="detail">
+        <img :src="item.img" alt="" />
+        <div class="content"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-  .container {
+.container {
+  width: 100%;
+  min-width: 1024px;
+  background-color: #4e7ca1;
+  .inner {
     width: 85vw;
+    height: 100%;
+    margin: 0 auto;
     min-width: 1024px;
-    height: 1024px;
-    margin: 100px auto;
-    border: 1px solid black;
+    .header {
+      height: 150px;
+      line-height: 150px;
+      color: var(--color);
+      font-size: var(--midFontSize);
+    }
+    .detail {
+      display: flex;
+      img {
+        width: 400px;
+        height: 600px;
+        border: 1px solid var(--color);
+        object-fit: cover;
+        object-position: 50% 20%;
+      }
+    }
   }
+}
 </style>
--- src/layout/header/Header.vue
+++ src/layout/header/Header.vue
@@ -1,27 +1,54 @@
 <script setup lang="ts">
 import { ref, reactive, onMounted } from 'vue';
+const items = reactive([
+  {
+    name: 'HOME',
+  },
+  {
+    name: 'ABOUT',
+  },
+]);
+
+const intros = reactive([
+  {
+    // img: '../../../public/introduce/1.jpg',
+    name: 'ESTELLE',
+  },
+  {
+    // img: '../../../public/introduce/2.png',
+    name: 'ISHIHARA SATOMI',
+  },
+  {
+    // img: '../../../public/introduce/3.png',
+    name: 'SONGYI',
+  },
+  {
+    // img: '../../../public/introduce/4.png',
+    name: 'KIMCHAEHYUN',
+  },
+]);
 </script>
 
 <template>
   <div class="header">
     <div class="nav">
-      <div class="logo">LOGO</div>
+      <div class="logo">XJD  IDOL</div>
       <div class="items">
         <ul>
-          <li>
+          <li v-for="item in items">
             <span>
-              <a href="#">HOME</a>
-            </span>
-          </li>
-          <li>
-            <span>
-              <a href="#">ABOUT</a>
+              <a href="#">{{ item.name }}</a>
             </span>
           </li>
         </ul>
       </div>
     </div>
-    <div class="introduce"></div>
+    <div class="introduce">
+      <div class="item" v-for="intro in intros">
+        <!-- <img :src="intro.img" alt="" /> -->
+        <p>{{ intro.name }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -71,8 +98,31 @@ import { ref, reactive, onMounted } from 'vue';
     width: 85vw;
     min-width: 1024px;
     margin: 0 auto;
-    height: calc(100% - 250px);
-    border: 1px solid #fff;
+    height: calc(100% - 200px);
+    border: 2px solid #fff;
+    overflow: hidden;
+
+    display: flex;
+    .item {
+      width: 25%;
+      position: relative;
+      img {
+        object-fit: cover;
+        object-position: 50% 20%;
+        position: absolute;
+      }
+      p {
+        width: 100%;
+        height: 50px;
+        line-height: 50px;
+        background-color: #4e7ca1;
+        text-align: center;
+        color: var(--color);
+        position: absolute;
+        bottom: 0;
+        border: 2px solid var(--color);
+      }
+    }
   }
 }
 </style>
